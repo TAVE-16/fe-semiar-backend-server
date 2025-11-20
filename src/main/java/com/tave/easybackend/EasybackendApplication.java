5fe009feb9150aba8b4d89e3da3e707df86b0f76
koreaioi <koreaioi@gachon.ac.kr>
2025-11-21 02:42:09 +0900

    chore: 초기 세팅

---
+++ src/main/java/com/tave/easybackend/EasybackendApplication.java
@@ -0,0 +1,13 @@
+package com.tave.easybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasybackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tave/easybackend/EasybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tave.easybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
